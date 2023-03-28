e551f609a0c4e5ddf436b30c02096d62fe7550fa
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2023-03-28 17:28:00 +0200

    hmtl resources

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,36 +6,36 @@ import { Info } from "../About/DeveloperData";
 // import useMediaQuery from "../../utilities/useMediaQuery";
 
 export default function Projects() {
-  const [projects, setProjects] = useState([]);
+  // const [projects, setProjects] = useState([]);
 
-  // const projects = [
-  //   {
-  //     name: "Portfolio-NextJs",
-  //     homepage: "https://test.com",
-  //     html_url: "https://test.com",
-  //   },
-  //   {
-  //     name: "Portfolio-Angular",
-  //     homepage: "https://test.com",
-  //     html_url: "https://test.com",
-  //   },
-  // ];
+  const projects = [
+    {
+      name: "Portfolio-NextJs",
+      homepage: "https://test.com",
+      html_url: "https://test.com",
+    },
+    {
+      name: "Portfolio-Angular",
+      homepage: "https://test.com",
+      html_url: "https://test.com",
+    },
+  ];
 
   // const smallView = useMediaQuery(0, "480px");
 
-  useEffect(() => {
-    console.log("hi");
-    const getProjects = async () => {
-      try {
-        const list = await octokit.request("GET /orgs/PhuocRenee/repos", {});
-        console.log(list.data);
-        setProjects(list.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getProjects();
-  }, []);
+  // useEffect(() => {
+  //   console.log("hi");
+  //   const getProjects = async () => {
+  //     try {
+  //       const list = await octokit.request("GET /orgs/PhuocRenee/repos", {});
+  //       console.log(list.data);
+  //       setProjects(list.data);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+  //   getProjects();
+  // }, []);
 
   return (
     <section id="Projects" style={{ backgroundColor: "#8691AE" }}>
