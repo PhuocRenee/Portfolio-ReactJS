d57cdb073c21c11f8bd03fa71e03daafdd587be0
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2023-03-28 17:19:01 +0200

    octokit implemented

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,14 @@
 import { Typography, Container, Card, CardContent } from "@mui/material";
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { octokit } from "../../utilities/octokit";
 import ProjectCard from "./ProjectCard";
 import { Info } from "../About/DeveloperData";
 // import useMediaQuery from "../../utilities/useMediaQuery";
 
 export default function Projects() {
-  // const [projects, setProjects] = useState([
+  const [projects, setProjects] = useState([]);
+
+  // const projects = [
   //   {
   //     name: "Portfolio-NextJs",
   //     homepage: "https://test.com",
@@ -16,34 +19,23 @@ export default function Projects() {
   //     homepage: "https://test.com",
   //     html_url: "https://test.com",
   //   },
-  // ]);
+  // ];
 
-  const projects = [
-    {
-      name: "Portfolio-NextJs",
-      homepage: "https://test.com",
-      html_url: "https://test.com",
-    },
-    {
-      name: "Portfolio-Angular",
-      homepage: "https://test.com",
-      html_url: "https://test.com",
-    },
-  ];
   // const smallView = useMediaQuery(0, "480px");
-  // useEffect(() => {
-  //   console.log("hi");
-  //   const getProjects = async () => {
-  //     try {
-  //       const list = await octokit.request("GET /orgs/PhuocRenee/repos", {});
-  //       console.log(list.data);
-  //       setProjects(list.data);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-  //   getProjects();
-  // }, []);
+
+  useEffect(() => {
+    console.log("hi");
+    const getProjects = async () => {
+      try {
+        const list = await octokit.request("GET /orgs/PhuocRenee/repos", {});
+        console.log(list.data);
+        setProjects(list.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getProjects();
+  }, []);
 
   return (
     <section id="Projects" style={{ backgroundColor: "#8691AE" }}>
